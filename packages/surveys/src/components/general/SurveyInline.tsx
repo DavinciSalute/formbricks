7542da128a27ae2e50b07f20cf3d81ecca95e78e
Bruno S. <b.scopelliti@davinci.care>
2024-12-04 14:29:28 +0100

    feat: add i18n module to survey

--- packages/surveys/src/components/general/SurveyInline.tsx
+++ packages/surveys/src/components/general/SurveyInline.tsx
@@ -1,10 +1,18 @@
+import { translations } from "@/lib/translate";
+import { IntlProvider } from "preact-i18n";
 import { SurveyInlineProps } from "@formbricks/types/formbricksSurveys";
 import { Survey } from "./Survey";
 
 export const SurveyInline = (props: SurveyInlineProps) => {
+  const lang = (
+    props.languageCode in translations ? props.languageCode : "default"
+  ) as keyof typeof translations;
+
   return (
-    <div id="fbjs" className="formbricks-form h-full w-full">
-      <Survey {...props} />
-    </div>
+    <IntlProvider definition={translations[lang]}>
+      <div id="fbjs" className="formbricks-form h-full w-full">
+        <Survey {...props} />
+      </div>
+    </IntlProvider>
   );
 };
--- packages/surveys/src/components/general/SurveyModal.tsx
+++ packages/surveys/src/components/general/SurveyModal.tsx
@@ -1,8 +1,8 @@
 import { Modal } from "@/components/wrappers/Modal";
+import { translations } from "@/lib/translate";
+import { IntlProvider } from "preact-i18n";
 import { useState } from "preact/hooks";
-
 import { SurveyModalProps } from "@formbricks/types/formbricksSurveys";
-
 import { Survey } from "./Survey";
 
 export const SurveyModal = ({
@@ -37,41 +37,45 @@ export const SurveyModal = ({
 
   const highlightBorderColor = styling?.highlightBorderColor?.light || null;
 
+  const lang = (languageCode in translations ? languageCode : "default") as keyof typeof translations;
+
   return (
-    <div id="fbjs" className="formbricks-form">
-      <Modal
-        placement={placement}
-        clickOutside={clickOutside}
-        darkOverlay={darkOverlay}
-        highlightBorderColor={highlightBorderColor}
-        isOpen={isOpen}
-        onClose={close}>
-        <Survey
-          survey={survey}
-          isBrandingEnabled={isBrandingEnabled}
-          onDisplay={onDisplay}
-          getSetIsResponseSendingFinished={getSetIsResponseSendingFinished}
-          onResponse={onResponse}
-          languageCode={languageCode}
-          onClose={close}
-          onFinished={() => {
-            onFinished();
-            setTimeout(() => {
-              if (!survey.redirectUrl) {
-                close();
-              }
-            }, 3000); // close modal automatically after 3 seconds
-          }}
-          onRetry={onRetry}
-          getSetIsError={getSetIsError}
-          onFileUpload={onFileUpload}
-          isRedirectDisabled={isRedirectDisabled}
-          responseCount={responseCount}
-          styling={styling}
-          isCardBorderVisible={!highlightBorderColor}
-          clickOutside={placement === "center" ? clickOutside : undefined}
-        />
-      </Modal>
-    </div>
+    <IntlProvider definition={translations[lang]}>
+      <div id="fbjs" className="formbricks-form">
+        <Modal
+          placement={placement}
+          clickOutside={clickOutside}
+          darkOverlay={darkOverlay}
+          highlightBorderColor={highlightBorderColor}
+          isOpen={isOpen}
+          onClose={close}>
+          <Survey
+            survey={survey}
+            isBrandingEnabled={isBrandingEnabled}
+            onDisplay={onDisplay}
+            getSetIsResponseSendingFinished={getSetIsResponseSendingFinished}
+            onResponse={onResponse}
+            languageCode={languageCode}
+            onClose={close}
+            onFinished={() => {
+              onFinished();
+              setTimeout(() => {
+                if (!survey.redirectUrl) {
+                  close();
+                }
+              }, 3000); // close modal automatically after 3 seconds
+            }}
+            onRetry={onRetry}
+            getSetIsError={getSetIsError}
+            onFileUpload={onFileUpload}
+            isRedirectDisabled={isRedirectDisabled}
+            responseCount={responseCount}
+            styling={styling}
+            isCardBorderVisible={!highlightBorderColor}
+            clickOutside={placement === "center" ? clickOutside : undefined}
+          />
+        </Modal>
+      </div>
+    </IntlProvider>
   );
 };
