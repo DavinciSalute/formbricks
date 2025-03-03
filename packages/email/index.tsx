7e1d910921e10e41189dd6963a09108abc7ce284
Bruno S. <b.scopelliti@davinci.care>
2025-03-03 16:09:48 +0100

    chore: add debug log

--- packages/email/index.tsx
+++ packages/email/index.tsx
@@ -149,6 +149,10 @@ export const sendVerificationEmail = async (user: TEmailUser) => {
   const verificationRequestLink = `${WEBAPP_URL}/auth/verification-requested?email=${encodeURIComponent(
     user.email
   )}`;
+
+  console.log("verificationRequestLink", verificationRequestLink);
+  console.log("verifyLink", verifyLink);
+
   await sendEmail({
     to: user.email,
     subject: "Please verify your email to use Formbricks",
