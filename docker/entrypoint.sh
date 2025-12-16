6bb9a04e9025874ff187b1a244339a8965199090
Paolo Leggio - DaVinci Care <p.leggio@davinci.care>
2025-12-16 10:03:13 +0100

    fix : supercronic logs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -23,7 +23,7 @@ trap cleanup TERM INT
 
 # Avvia supercronic in background per i cronjob
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] Avvio supercronic per i cronjob..."
-supercronic /app/docker/cronjobs &
+supercronic -passthrough-logs  -json  /app/docker/cronjobs &
 SUPERCRONIC_PID=$!
 
 # Attendi un momento per assicurarsi che supercronic sia avviato
