10b9439d3635fc25a2ba0c235184df429f85eb9f
Paolo Leggio - DaVinci Care <p.leggio@davinci.care>
2026-03-02 16:41:09 +0100

    fix: use @trieb.work/nextjs-turbo-redis-cache

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -170,11 +170,10 @@ const nextConfig = {
   },
 };
 
-// set custom cache handler
-// Not longer supported in Next 15.0.7
-// if (process.env.CUSTOM_CACHE_DISABLED !== "1") {
-//   nextConfig.cacheHandler = require.resolve("./cache-handler.mjs");
-// }
+
+if (process.env.REDIS_URL) {
+  nextConfig.cacheHandler = require.resolve("@trieb.work/nextjs-turbo-redis-cache");
+}
 
 // set actions allowed origins
 if (process.env.WEBAPP_URL) {
